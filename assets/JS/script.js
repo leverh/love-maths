63b92425db87b555eeecb89557d0c33637cb81db
leverh <leverh@gmail.com>
2023-01-23 14:08:43 +0000

    Add additions function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,10 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 
@@ -21,9 +24,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * main game loop is called when script is first loaded 
  * and after the user answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
+        
 }
 
 function checkAnswer() {
@@ -42,8 +53,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
